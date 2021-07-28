829c5323e8dc9e3841d30bcf2787a4ea46a8cb08
sk <1724271@donga.ac.kr>
2021-07-28 14:40:01 +0900

    complete

---
+++ 2743/main.cpp
@@ -0,0 +1,11 @@
+//2743
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    string input;
+    cin>>input;
+    cout << input.length();
+    return 0;
+}
